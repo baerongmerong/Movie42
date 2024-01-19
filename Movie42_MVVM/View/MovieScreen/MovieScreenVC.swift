19fff736bfa779440edf311cbddf640bd45d31c7
baerongmerong <151704516+baerongmerong@users.noreply.github.com>
2024-01-19 16:13:26 +0900

    Screen code 수정

--- Movie42_MVVM/View/MovieScreen/MovieScreenVC.swift
+++ Movie42_MVVM/View/MovieScreen/MovieScreenVC.swift
@@ -1,59 +1,39 @@
-//
-//  MovieScreenVC.swift
-//  Movie42_MVVM
-//
-//  Created by Bae on 1/18/24.
-//
-
 import Foundation
 import UIKit
 
-class ListCell: UICollectionViewCell {
-    @IBOutlet weak var movieCover: UIImageView!
-    @IBOutlet weak var movieTitle: UILabel!
-    
-}
-
 class MovieScreenViewController: UIViewController {
     
-    let viewModel = MovieViewModel()
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let movieScreenVM = MovieScreenVM()
        
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.dataSource = self
+        movieScreenVM.fetchData()
+        
     }
 }
 
 extension MovieScreenViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return viewModel.listCount
+        return movieScreenVM.itemsCount()
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? ListCell else {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? ScreenCollectionViewCell else {
             return UICollectionViewCell()
         }
         
-        let movie = viewModel.getTheMovie(at: indexPath.item)
-//        cell.updateView(info: movie)
-    
+        let movie = movieScreenVM.item(at: indexPath.item)
+        cell.configure(with: movie)
+        
         return cell
     }
 }
 
-extension MovieScreenViewController: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "show", sender: indexPath.item)
-    }
-}
-
-//extension MovieScreenViewController: UICollectionViewDelegateFlowLayout {
-//    func collectionView(_ collectionVoew: UICollectionView, layout collectionViewLayout:UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        let itemSpacing: CGFloat = 20
-//        let textAreaHeight: CGFloat = 25
-//
-//        let width: CGFloat =
-//        let height: CGFloat =
-//
-//        return CGSize(width: width, height: height)
-//
+//extension MovieScreenViewController: UICollectionViewDelegate {
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        performSegue(withIdentifier: "show", sender: indexPath.item)
 //    }
 //}
