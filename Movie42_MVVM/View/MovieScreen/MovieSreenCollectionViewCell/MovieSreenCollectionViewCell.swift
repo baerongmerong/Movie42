ea9791bee8af8e4b7c647ceeef0f5f1f67b70d42
mirae0312 <ekdtls58@gmail.com>
2024-01-22 00:09:06 +0900

    commit - last

---
+++ Movie42_MVVM/View/MovieScreen/MovieSreenCollectionViewCell/MovieSreenCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSreenCollectionViewCell.swift
+//  Movie42_MVVM
+//
+//  Created by mirae on 1/21/24.
+//
+
+import UIKit
+
+class MovieSreenCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Movie42_MVVM/View/MovieScreen/MovieSreenCollectionViewTableCell/MovieSreenCollectionViewTableCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieSreenCollectionViewTableCell.swift
+//  Movie42_MVVM
+//
+//  Created by mirae on 1/21/24.
+//
+
+import UIKit
+
+class MovieSreenCollectionViewTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
